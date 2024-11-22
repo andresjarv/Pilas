e060d045d74143fc1c425ff36b1b469a72682bbc
andresjarv <andresjarv95@gmail.com>
2024-11-22 14:57:02 -0500

    pila agrega elementos factorial

--- main.java
+++ main.java
@@ -1,7 +1,66 @@
 package Pilas;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class main{
     public static void main(String[] args) {
+        punto9 p9 = new punto9();
+        Scanner leer = new Scanner(System.in);
+        Stack<Integer> stack = new Stack();
+        int tamano = 0;
+        int opt = 0;
+        boolean bandera = true;        
+        
+        while (bandera) {
+            System.out.println("Ingrese la opcion que desea: ");
+            System.out.println("1. Llenar pila");
+            System.out.println("2. Mostrar pila");
+            System.out.println("3. Ordenar pila");
+            System.out.println("4. Buscar y eliminar un elemento");
+            System.out.println("5. salir");
+            while (!leer.hasNextInt()) {
+                System.out.println("Ingrese una opcion valida");
+                leer.next();
+            }
+            opt = leer.nextInt();
+            switch (opt) {
+                case 1:
+                    System.out.println("Ingrese el tamaño de la Pila Aleatoria: ");
+                    tamano = leer.nextInt();
+                    p9.LlenarPila(stack, tamano);
+                    break;
+
+                case 2:
+                    p9.mostrarPila(stack);
+                    break;
+
+                case 3:
+                    p9.OrdenarPila(stack);
+                    System.out.println("Pila ordenada correctamente.");                    
+                    break;
+
+                case 4:
+                    System.out.println("Ingrese el elemento a eliminar: ");
+                    int elemento = leer.nextInt();
+                    String mensaje = p9.BuscarElemento(stack, elemento);
+                    System.out.println(mensaje);
+                    
+                    p9.mostrarPila(stack);
+                
+                    
+                    break;
+                case 5:
+                    System.out.println("saliendo del programa");
+                    bandera = false;
+                    break;
+
+                default:
+                    System.out.println("pagina en mantenimiento");
+                    break;
+            }
+        }
+        
         
     }
 }
--- punto9.java
+++ punto9.java
@@ -1,7 +1,7 @@
 // cree un programa que tome números aleatoriamente y que calcule la factorial y 
 //ordene de manera ascendente la pila
 
-// 1: ordene de manera descendiente la pila
+// 1: ordene de manera descendente la pila
 
 // 2: que busque uno de los registros y lo elimine
 
@@ -9,13 +9,60 @@
 
 package Pilas;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
-public class punto9{
-    public Stack<Integer> LlenarPila(Stack<Integer> pila){
+public class punto9 {
+    public Stack<Integer> LlenarPila(Stack<Integer> pila, int tamano) {
 
-        
+        for (int i = 0; i < tamano; i++) {
+            pila.push(Factorial((int) (Math.random() * 10 + 1)));
+        }
+        System.out.println("Se lleno la pila con el Factorial de cada elemento.");
         return pila;
+    }
 
+    public int Factorial(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public void mostrarPila(Stack<Integer> pila) {
+        System.out.println(pila);
+
+    }
+
+    public Stack<Integer> OrdenarPila(Stack<Integer> pila) {
+        List<Integer> lista = new ArrayList<>(pila);
+        Collections.sort(lista, Collections.reverseOrder());
+        pila.clear();
+        pila.addAll(lista);
+        return pila;
+    }
+
+    public String BuscarElemento(Stack<Integer> pila, int elemento) {
+
+        String mensaje = "";
+        Stack<Integer> pilaAux = new Stack<>();
+
+        while (!pila.isEmpty()) {
+            int actual = pila.pop();
+            if (actual != elemento) {
+                pilaAux.push(actual);
+            } else {                
+                break;
+            }
+        }
+
+        while (!pilaAux.isEmpty()) {
+            pila.push(pilaAux.pop());
+        }
+        
+        return mensaje;
     }
 }
